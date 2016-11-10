aafcd07e0de631ccefa1bbbd47ea63b3fb6920c4
Eric Cote <eric@coteexpert.com>
2016-11-09 19:02:53 -0500

    Modifications

--- TestVarie/Scripts/film.js
+++ TestVarie/Scripts/film.js
@@ -1,8 +1,11 @@
 ﻿
-var p = new Object();
-p.apiKey="626559fd3799f9466c64ea0daa420543";
-p.baseUrl="";
 
+(function () {
+
+
+var p = new Object();
+p.apiKey = "626559fd3799f9466c64ea0daa420543";
+p.baseUrl = "";
 
 p.get = function (id) {
     return document.getElementById(id);
@@ -11,41 +14,41 @@ p.get = function (id) {
 p.getMovies = function () {
     //    var xhr = new XMLHttpRequest();
     //    xhr.open("GET",
-    //             "http://odata.netflix.com/Catalog/Genres('Mexican%20Movies')/Titles/?$top=5&$callback=callback&$format=json",
+    //             "http://odata.netflix.com/Catalog/Genres('Mexican%20Movies')/Titles/?$top=5&$callback=callback&$format=films",
     //             false);
     //    xhr.send();
     var selected = p.get("ListeCategories").value;
-    var getGenres="https://api.themoviedb.org/3/discover/movie?api_key=" + p.apiKey + "&with_genres=" + selected ;
+    var getGenres = "https://api.themoviedb.org/3/discover/movie?api_key=" + p.apiKey + "&with_genres=" + selected;
     var req = new XMLHttpRequest();
     req.open('GET', getGenres);
     req.setRequestHeader('Accept', 'application/json');
     req.onreadystatechange = function () {
-       if (this.readyState === 4) {
-           console.log(this.responseText);
-          var list = JSON.parse(this.responseText);
-          p.afficherFilms(list);
-          
-       }
+        if (this.readyState === 4) {
+            console.log(this.responseText);
+            var list = JSON.parse(this.responseText);
+            p.afficherFilms(list);
+
+        }
     };
 
     req.send();
-    
-    
+
+
 };
 
 
-p.afficherFilms = function (json) {
+p.afficherFilms = function (films) {
     var para, h3, br, img;
     p.get("main").textContent = "";
-    for (var i = 0; i < json.results.length; i += 1) {
+    for (var i = 0; i < films.results.length; i += 1) {
         img = document.createElement('img');
         para = document.createElement('p');
         h3 = document.createElement('h3');
         br = document.createElement('br');
 
-        para.textContent = json.results[i].overview;
-        h3.textContent = json.results[i].title;
-        img.src = p.baseUrl + "w185" + json.results[i].poster_path;
+        para.textContent = films.results[i].overview;
+        h3.textContent = films.results[i].title;
+        img.src = p.baseUrl + "w185" + films.results[i].poster_path;
         img.className = "gauche";
         br.className = "clear";
 
@@ -58,22 +61,22 @@ p.afficherFilms = function (json) {
 };
 
 
-p.initConfig = function()  {
-    var getConfig="https://api.themoviedb.org/3/configuration?api_key=" + p.apiKey;
+p.initConfig = function () {
+    var getConfig = "https://api.themoviedb.org/3/configuration?api_key=" + p.apiKey;
     var req = new XMLHttpRequest();
     req.open('GET', getConfig);
     req.setRequestHeader('Accept', 'application/json');
     console.log(req.timeout);
     req.onreadystatechange = function () {
-       if (this.readyState === 4) {
-          var reponse = JSON.parse(this.responseText);
-          p.baseUrl = reponse.images.base_url;
-       }
+        if (this.readyState === 4) {
+            var reponse = JSON.parse(this.responseText);
+            p.baseUrl = reponse.images.base_url;
+        }
     };
 
     req.send();
 };
-    
+
 
 
 p.pageLoad = function () {
@@ -82,26 +85,29 @@ p.pageLoad = function () {
 
     p.initConfig();
 
-    var getGenres="https://api.themoviedb.org/3/genre/movie/list?api_key=" + p.apiKey;
+    var getGenres = "https://api.themoviedb.org/3/genre/movie/list?api_key=" + p.apiKey;
     var req = new XMLHttpRequest();
     req.open('GET', getGenres);
     req.setRequestHeader('Accept', 'application/json');
-  
+
     req.onreadystatechange = function () {
-       if (this.readyState === 4) {
-          var list = JSON.parse(this.responseText);
-          var op;
-          for (var i = 0; i < list.genres.length; i++) {
-             op = document.createElement("option");
-             op.textContent = list.genres[i].name;
-             op.value = list.genres[i].id;
-             p.get("ListeCategories").appendChild(op);
-          }
-       }
+        if (this.readyState === 4) {
+            var genres = JSON.parse(this.responseText);
+            var op;
+            for (var i = 0; i < genres.genres.length; i++) {
+                op = document.createElement("option");
+                op.textContent = genres.genres[i].name;
+                op.value = genres.genres[i].id;
+                p.get("ListeCategories").appendChild(op);
+            }
+        }
     };
 
     req.send();
 };
 
 
-window.addEventListener("load", p.pageLoad)
+window.addEventListener("DOMContentLoaded", p.pageLoad)
+
+
+})();
